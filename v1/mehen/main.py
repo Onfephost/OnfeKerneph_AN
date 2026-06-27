33519a36f95b3a1cb57fa0fdf4aeb46f3f4f074a
Onfephost <onfephost@gmail.com>
2026-06-27 09:00:09 +0000

    new file:   v1/mehen/__pycache__/train_datas.cpython-312.pyc 	modified:   v1/mehen/core/__pycache__/embedding.cpython-312.pyc 	modified:   v1/mehen/main.py 	new file:   v1/mehen/train_datas.py

--- v1/mehen/main.py
+++ v1/mehen/main.py
@@ -1,89 +1,7 @@
 from core.network import Network
+from train_datas import vocab_size,data,encode,id2word
 
-# VOCAB
-words = [
-    "how","are","you","hi","hello","ai","i","am","they","we",
-    "what","is","your","name","my","good","bad","fine","thanks",
-    "who","esc",
-]
-numbers = list(range(0,len(words)))
-word2id = {a:b for a,b in zip(words,numbers)}
-
-id2word = {v: k for k, v in word2id.items()}
-vocab_size = len(word2id)
-
-def encode(text):
-    return [word2id[w] for w in text.split() if w in word2id]
-
-# TRAINING DATA
-data = [
-    # how are you
-    ([0], 1),
-    ([0,1], 2),
-    ([0,1,2], 20),
-    # hi -> how are you
-    ([3], 0),
-    ([3,0], 1),
-    ([3,0,1], 2),
-    ([3,0,1,2], 20),
-
-    # hello -> hi
-    ([4], 3),
-    ([4,3], 20),
-
-    # i am ai
-    ([6], 7),
-    ([6,7], 5),
-    ([6,7,5], 20),
-
-    # they are good
-    ([8], 1),
-    ([8,1], 15),
-    ([8,1,15], 20),
-
-    # we are fine
-    ([9], 1),
-    ([9,1], 17),
-    ([9,1,17], 20),
-
-    # what is your name
-    ([10], 11),
-    ([10,11], 12),
-    ([10,11,12], 13),
-    ([10,11,12,13], 20),
-
-    # my name ai
-    ([14], 13),
-    ([14,13], 5),
-    ([14,13,5], 20),
-
-    # how are you -> fine
-    ([0,1,2], 17),
-    ([0,1,2,17], 20),
-
-    # how are you -> good
-    ([0,1,2], 15),
-    ([0,1,2,15], 20),
-
-    # how are you -> bad
-    ([0,1,2], 16),
-    ([0,1,2,16], 20),
-
-    # thanks
-    ([18], 20),
-
-    # who are you
-    ([19], 1),
-    ([19,1], 2),
-    ([19,1,2], 20),
-]
-
-
-
-
-
-# MODEL
-def createModel(vocab_size):
+def createModel(vocab_size,epochCount=2000,s=100):
     net = Network(vocab_size=vocab_size,
     dim=64,lr=0.005,
     ff_hidden=128,)
@@ -91,8 +9,8 @@ def createModel(vocab_size):
     print("=== TRAINING ===")
 
     loss = net.train(data,
-    epochs=2500,
-    step=50,
+    epochs=epochCount,
+    step=s,
     )
 
     net.save("model.qai")
